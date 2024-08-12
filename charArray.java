2509201dd09fb3b808e888b4905d925d37c1a421
CodewithKunal024 <shaw.kunal024@gmail.com>
2024-08-13 00:25:07 +0530

    Update charArray.java

--- charArray.java
+++ charArray.java
@@ -1,3 +1,23 @@
-public class charArray {
+import java.util.Scanner;
 
+public class charArray {
+    public String transformation(String a){
+        char array[] = a.toCharArray();
+        for(int i=0;i<a.length();i++ ){
+            if(array[i]=='a'){
+                array[i]='b';
+            }
+            else if(array[i]=='b'){
+                array[i]='a';
+            }
+        }
+        return new String(array);
+    }
+public static void main(String args[]){
+Scanner sc= new Scanner(System.in);
+String a ="aabbcc";
+ charArray obj= new charArray();
+ System.out.println(obj.transformation(a));
+    }
 }
+
